b1b72f51985a21f95221dcd2484d9886470b9ebd
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2024-07-29 23:06:20 -0700

    fixed one bug

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -3,9 +3,12 @@ const express = require('express');
 const router = express.Router();
 const { getJournalEntry, saveJournalEntry, getJournalEntryDates } = require('../controllers/journalController');
 
+//this route MUST come before get to /:id/:date, as routes are processed from top to bottom
+router.get('/:id/dates', getJournalEntryDates);
+
 router.get('/:id/today', getJournalEntry);
 router.post('/:id/today', saveJournalEntry);
 router.get('/:id/:date', getJournalEntry);
-router.get('/:id/dates', getJournalEntryDates);
+
 
 module.exports = router;
